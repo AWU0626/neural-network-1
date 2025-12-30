3c2518b742b5c5f39a19541bf60765644aef1d1c
awu0626 <aaronwu321@gmail.com>
2025-12-29 20:23:02 -0500

    headers for neuron.h

--- Neuron.h
+++ Neuron.h
@@ -1,5 +1,27 @@
 #ifndef NEURON_H
 #define NEURON_H
 
+#include <algorithm>
+#include <vector>
+class Neuron {
+  private:
+    std::vector<double> weights;
+    double bias;
+
+  public:
+    Neuron(const std::vector<double>& w0, double b0);
+
+    // activation functions
+    double ReLU(double x);
+    double tanh(double x);
+    double sigmoid(double x);
+
+    // forward propogation
+    double forward(const std::vector<double>& input, double (*activationFunction)(double));
+
+    // getters
+    std::vector<double> getWeights() const;
+    double getBias() const;
+};
 
 #endif // Neuron.h
